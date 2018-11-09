a2d58467f4c7b0c9d654bfe7f417bebb2f8535e2
807924995@qq.com <qq807924995>
2018-11-09 11:32:03 +0800

    1.添加gitignore 2.修改支持spring boot2.0 f版本的cloud

--- transaction-springcloud/src/main/java/com/codingapi/tx/springcloud/listener/ServerListener.java
+++ transaction-springcloud/src/main/java/com/codingapi/tx/springcloud/listener/ServerListener.java
@@ -4,30 +4,38 @@ import com.codingapi.tx.listener.service.InitService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerInitializedEvent;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
 
 @Component
-public class ServerListener implements ApplicationListener<EmbeddedServletContainerInitializedEvent> {
+public class ServerListener implements  ApplicationListener<ApplicationStartedEvent> {
 
     private Logger logger = LoggerFactory.getLogger(ServerListener.class);
 
+    @Value("${spring.port}")
     private int serverPort;
 
     @Autowired
     private InitService initService;
 
-    @Override
-    public void onApplicationEvent(EmbeddedServletContainerInitializedEvent event) {
-        logger.info("onApplicationEvent -> onApplicationEvent. "+event.getEmbeddedServletContainer());
-        this.serverPort = event.getEmbeddedServletContainer().getPort();
-
-        initService.start();
-    }
+//    @Override
+//    public void onApplicationEvent(EmbeddedServletContainerInitializedEvent event) {
+//        logger.info("onApplicationEvent -> onApplicationEvent. "+event.getEmbeddedServletContainer());
+//        this.serverPort = event.getEmbeddedServletContainer().getPort();
+//
+//        initService.start();
+//    }
 
     public int getPort() {
         return this.serverPort;
+    }
+
+    @Override
+    public void onApplicationEvent(ApplicationStartedEvent event) {
+        logger.info("onApplicationEvent -> onApplicationEvent. "+event);
+        initService.start();
     }
 }
